004e17eee6e3420375928a9ffc4f4a2e3976a4a2
Saad Mirza <saadmirza009@gmail.com>
2020-06-28 15:34:22 +0500

    add booking wizards view

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from booking.models import Booking
+from booking.models import Booking, Address
 from django.forms import ModelForm
 from django import forms
 from django.utils.translation import ugettext_lazy as _
@@ -7,6 +7,10 @@ from crispy_forms.layout import Submit, Layout, Field
 
 
 class BookingForm(ModelForm):
+    address = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Enter Street Address', 'onFocus': 'geolocate()'}))
+
     class Meta:
         model = Booking
         fields = ['start_time', 'end_time', 'address', 'customer', 'service', 'staff', 'access_arrangments', 'key_no',
@@ -14,4 +18,17 @@ class BookingForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(BookingForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+
+class AddressForm(ModelForm):
+    lat = forms.CharField(widget=forms.HiddenInput(), required=True)
+    long = forms.CharField(widget=forms.HiddenInput(), required=True)
+
+    class Meta:
+        model = Address
+        fields = ['lat', 'long', 'street_name', 'details', 'suburb', 'state', 'postcode', 'country']
+
+    def __init__(self, *args, **kwargs):
+        super(AddressForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
--- booking/views.py
+++ booking/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, DetailView, FormView, UpdateView, Cre
 from django.contrib.auth.mixins import LoginRequiredMixin
 from booking.models import Booking
 from services.forms import ServiceForm
-from booking.forms import BookingForm
+from booking.forms import BookingForm, AddressForm
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -24,6 +24,11 @@ class BookingCreateView(CreateView, FormView):
     model = Booking
     form_class = BookingForm
     template_name = 'admin/bookings/add.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookingCreateView, self).get_context_data(**kwargs)
+        context['AddressForm'] = AddressForm()
+        return context
 
 
 class BookingsUpdate(UpdateView):
